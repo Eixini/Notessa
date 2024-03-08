4fd86a5dda670f5fb2cd107d4f40f9e68e5660b0
Eixini <eixi16@gmail.com>
2024-03-09 00:08:14 +0300

    Add class for checking application data directories

---
+++ Notessa/settings/directory_checker.py
@@ -0,0 +1,53 @@
+from PySide6.QtCore import QDir, QStandardPaths
+
+
+class DirectoryChecker():
+
+    def __init__(self):
+        self.applicationDataLocation = str(QStandardPaths.writableLocation(QStandardPaths.AppConfigLocation))
+
+    def application_directory_checker(self):
+        appDir = QDir()
+        if not QDir(self.applicationDataLocation).exists():
+            appDir.mkdir(self.applicationDataLocation)
+            print(f'Directory create: {self.applicationDataLocation}')
+
+
+    def notes_directory_checker(self):
+        appDir = QDir()
+        notesPath = f'{self.applicationDataLocation}{QDir.separator()}Notes'
+        if not QDir(notesPath).exists():
+            appDir.mkdir(notesPath)
+
+
+    def text_notes_directory_checker(self):
+        appDir = QDir()
+        notesPath = f'{self.applicationDataLocation}{QDir.separator()}Notes{QDir.separator()}TextNotes'
+        if not QDir(notesPath).exists():
+            appDir.mkdir(notesPath)
+
+
+    def voice_notes_directory_checker(self):
+        appDir = QDir()
+        notesPath = f'{self.applicationDataLocation}{QDir.separator()}Notes{QDir.separator()}VoiceNotes'
+        if not QDir(notesPath).exists():
+            appDir.mkdir(notesPath)
+
+
+    def video_notes_directory_checker(self):
+        appDir = QDir()
+        notesPath = f'{self.applicationDataLocation}{QDir.separator()}Notes{QDir.separator()}VideoNotes'
+        if not QDir(notesPath).exists():
+            appDir.mkdir(notesPath)
+
+
+    def paint_notes_directory_checker(self):
+        appDir = QDir()
+        notesPath = f'{self.applicationDataLocation}{QDir.separator()}Notes{QDir.separator()}PaintNotes'
+        if not QDir(notesPath).exists():
+            appDir.mkdir(notesPath)
+
+
+    def text_notes_directory(self) -> str:
+        return f'{self.applicationDataLocation}{QDir.separator()}Notes{QDir.separator()}TextNotes'
+
