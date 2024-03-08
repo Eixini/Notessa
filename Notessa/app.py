fbd9c16020171ecc726501a82be159eadf297b8a
Eixini <eixi16@gmail.com>
2024-03-09 00:09:08 +0300

    Add checking directory existence

--- Notessa/app.py
+++ Notessa/app.py
@@ -1,12 +1,25 @@
 import sys
 from PySide6.QtWidgets import QApplication, QWidget
 from main_window import MainWindow
+from settings.directory_checker import DirectoryChecker
+
 
 def main():
     app = QApplication(sys.argv)
+    app.setApplicationName('Notessa')
+    app.setApplicationVersion('0.1')
 
     window = MainWindow()
     window.show()
+
+    # Checking and initializing note directories
+    dirChecker = DirectoryChecker()
+    dirChecker.application_directory_checker()
+    dirChecker.notes_directory_checker()
+    dirChecker.text_notes_directory_checker()
+    dirChecker.voice_notes_directory_checker()
+    dirChecker.video_notes_directory_checker()
+    dirChecker.paint_notes_directory_checker()
 
     app.exec()
 
