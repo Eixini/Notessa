a09669ff35048f3bf3101e291b7082303ae7880a
Eixini <eixi16@gmail.com>
2024-03-08 20:54:06 +0300

    Change application initialization

--- Notessa/app.py
+++ Notessa/app.py
@@ -2,9 +2,13 @@ import sys
 from PySide6.QtWidgets import QApplication, QWidget
 from main_window import MainWindow
 
-app = QApplication(sys.argv)
+def main():
+    app = QApplication(sys.argv)
 
-window = MainWindow()
-window.show()
+    window = MainWindow()
+    window.show()
 
-app.exec()
+    app.exec()
+
+if __name__ == '__main__':
+    main()
