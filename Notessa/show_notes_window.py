e25d50057306cb96ef2f1a8533e7622ad7731f40
Eixini <eixi16@gmail.com>
2024-03-09 23:51:40 +0300

    Add notes list view

---
+++ Notessa/show_notes_window.py
@@ -0,0 +1,50 @@
+from PySide6.QtWidgets import QDialog, QWidget, QHeaderView, QAbstractItemView
+from PySide6.QtGui import QIcon, QPixmap
+from PySide6.QtCore import QEasingCurve, QPropertyAnimation, Qt, Slot, QFile, QDateTime, QDir, QModelIndex
+from Notessa.windows.ui_shownoteswindow import Ui_ShowNotesWindow
+from Notessa.model.NotesModel import NotesModel
+from Notessa.settings.directory_checker import DirectoryChecker
+from Notessa.NoteItemDelegate import NoteItemDelegate
+from Notessa.note_windows.ShowTextNoteWindow import ShowTextNoteWindow
+import Notessa.rc_icons
+
+
+class ShowNotesWindow(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_ShowNotesWindow()
+        self.ui.setupUi(self)
+
+        # Model
+        self.noteModel = NotesModel()
+        self.ui.notesTableView.setModel(self.noteModel)
+
+        #TableView
+        self.ui.notesTableView.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeMode.Stretch)
+        self.ui.notesTableView.setItemDelegateForColumn(0, NoteItemDelegate())
+        self.ui.notesTableView.setSelectionMode(QAbstractItemView.SingleSelection)
+        self.ui.notesTableView.setSelectionBehavior(QAbstractItemView.SelectRows)
+
+        #Signal-Slot
+        self.ui.toMainMenuButton.clicked.connect(lambda: self.accept())
+        self.ui.deleteNoteButton.clicked.connect(self.delete_note)
+        self.ui.showNoteButton.clicked.connect(self.show_note)
+
+
+    def delete_note(self):
+        index = self.ui.notesTableView.currentIndex()
+        self.noteModel.removeRows(index.row(), 1, index)
+
+
+    def show_note(self):
+        """Method for displaying a note.
+        The note type is obtained and based on this,
+        the appropriate representation is selected."""
+        index = self.ui.notesTableView.currentIndex()
+        noteType = self.noteModel.getNoteType(index)
+        data = self.noteModel.getCurrentData(index)
+        if noteType == 'txt':
+            textNoteWindow = ShowTextNoteWindow(data)
+            textNoteWindow.exec()
+        elif noteType == 'wav':
+            pass
---
+++ Notessa/windows/ui_shownoteswindow.py
@@ -0,0 +1,87 @@
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QDialog, QFrame, QGridLayout,
+    QHBoxLayout, QHeaderView, QPushButton, QSizePolicy,
+    QSpacerItem, QTableView, QVBoxLayout, QWidget)
+
+class Ui_ShowNotesWindow(object):
+    def setupUi(self, ShowNotesWindow):
+        if not ShowNotesWindow.objectName():
+            ShowNotesWindow.setObjectName(u"ShowNotesWindow")
+        ShowNotesWindow.setWindowModality(Qt.NonModal)
+        ShowNotesWindow.resize(1000, 674)
+        self.gridLayout = QGridLayout(ShowNotesWindow)
+        self.gridLayout.setObjectName(u"gridLayout")
+        self.verticalLayout = QVBoxLayout()
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.notesTableView = QTableView(ShowNotesWindow)
+        self.notesTableView.setObjectName(u"notesTableView")
+        self.notesTableView.setFrameShape(QFrame.NoFrame)
+        self.notesTableView.setFrameShadow(QFrame.Plain)
+        self.notesTableView.setMidLineWidth(1)
+        self.notesTableView.setHorizontalScrollBarPolicy(Qt.ScrollBarAsNeeded)
+        self.notesTableView.setGridStyle(Qt.NoPen)
+        self.notesTableView.setSortingEnabled(True)
+
+        self.verticalLayout.addWidget(self.notesTableView)
+
+        self.horizontalLayout = QHBoxLayout()
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.renameNoteButton = QPushButton(ShowNotesWindow)
+        self.renameNoteButton.setObjectName(u"renameNoteButton")
+        sizePolicy = QSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Minimum)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.renameNoteButton.sizePolicy().hasHeightForWidth())
+        self.renameNoteButton.setSizePolicy(sizePolicy)
+
+        self.horizontalLayout.addWidget(self.renameNoteButton)
+
+        self.showNoteButton = QPushButton(ShowNotesWindow)
+        self.showNoteButton.setObjectName(u"showNoteButton")
+        sizePolicy.setHeightForWidth(self.showNoteButton.sizePolicy().hasHeightForWidth())
+        self.showNoteButton.setSizePolicy(sizePolicy)
+
+        self.horizontalLayout.addWidget(self.showNoteButton)
+
+        self.deleteNoteButton = QPushButton(ShowNotesWindow)
+        self.deleteNoteButton.setObjectName(u"deleteNoteButton")
+
+        self.horizontalLayout.addWidget(self.deleteNoteButton)
+
+        self.horizontalSpacer = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
+
+        self.horizontalLayout.addItem(self.horizontalSpacer)
+
+        self.toMainMenuButton = QPushButton(ShowNotesWindow)
+        self.toMainMenuButton.setObjectName(u"toMainMenuButton")
+        sizePolicy.setHeightForWidth(self.toMainMenuButton.sizePolicy().hasHeightForWidth())
+        self.toMainMenuButton.setSizePolicy(sizePolicy)
+
+        self.horizontalLayout.addWidget(self.toMainMenuButton)
+
+
+        self.verticalLayout.addLayout(self.horizontalLayout)
+
+
+        self.gridLayout.addLayout(self.verticalLayout, 0, 0, 1, 1)
+
+
+        self.retranslateUi(ShowNotesWindow)
+
+        QMetaObject.connectSlotsByName(ShowNotesWindow)
+    # setupUi
+
+    def retranslateUi(self, ShowNotesWindow):
+        ShowNotesWindow.setWindowTitle(QCoreApplication.translate("ShowNotesWindow", u"Show notes", None))
+        self.renameNoteButton.setText(QCoreApplication.translate("ShowNotesWindow", u"Rename note", None))
+        self.showNoteButton.setText(QCoreApplication.translate("ShowNotesWindow", u"Show note", None))
+        self.deleteNoteButton.setText(QCoreApplication.translate("ShowNotesWindow", u"Delete note", None))
+        self.toMainMenuButton.setText(QCoreApplication.translate("ShowNotesWindow", u"Main menu", None))
+    # retranslateUi
+
