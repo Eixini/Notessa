7ef3c917d565bbdba9509b5b7bbc1cb8759630f3
Eixini <eixi16@gmail.com>
2024-03-09 23:50:23 +0300

    Add delegate to show icon for note types

---
+++ Notessa/NoteItemDelegate.py
@@ -0,0 +1,28 @@
+import PyQt6.QtWidgets
+from PySide6.QtWidgets import QStyledItemDelegate, QStyleOptionViewItem, QItemDelegate, QStyle
+from PySide6.QtCore import QModelIndex, QPoint, QRect, QModelRoleData, Qt
+from PySide6.QtGui import QIcon, QPixmap
+from Notessa import rc_icons
+
+class NoteItemDelegate(QStyledItemDelegate):
+
+    def paint(self, painter, option, index):
+        super(NoteItemDelegate, self).initStyleOption(option, index)
+        option.Position = QStyleOptionViewItem.Position.Left
+
+        noteType = index.data()
+
+        icons = {
+            'text': ':/resource/icons/text.png',
+            'voice': ':/resource/icons/microphone.png',
+            'video': ':/resource/icons/video.png',
+            'paint': ':/resource/icons/brush.png'
+        }
+        iconExample = QPixmap(':/resource/icons/text.png')
+        rect = QRect(QPoint(), iconExample.size().scaled(option.rect.size(), Qt.AspectRatioMode.KeepAspectRatio))
+        rect.moveCenter(option.rect.center())
+
+        if noteType == 'txt':
+            painter.drawPixmap(rect, QPixmap(icons['text']))
+        if noteType == 'wav':
+            painter.drawPixmap(rect, QPixmap(icons['voice']))
