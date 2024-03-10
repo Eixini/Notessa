b275cab57d18e072ff35643184bb4d44e70690e9
Eixini <eixi16@gmail.com>
2024-03-10 12:55:15 +0300

    Add method for counting number files and occupied size
    The method allows you to calculate the total number of notes in the notes directory and the amount of disk space they occupy. It also calculates for individual types of notes.

--- Notessa/settings/directory_checker.py
+++ Notessa/settings/directory_checker.py
@@ -1,10 +1,11 @@
-from PySide6.QtCore import QDir, QStandardPaths
+from PySide6.QtCore import QDir, QStandardPaths, QDirIterator
 
 
 class DirectoryChecker():
 
     def __init__(self):
         self.applicationDataLocation = str(QStandardPaths.writableLocation(QStandardPaths.AppConfigLocation))
+        self.size_notes_on_disk()
 
     def application_directory_checker(self):
         appDir = QDir()
@@ -12,13 +13,11 @@ class DirectoryChecker():
             appDir.mkdir(self.applicationDataLocation)
             print(f'Directory create: {self.applicationDataLocation}')
 
-
     def notes_directory_checker(self):
         appDir = QDir()
         notesPath = f'{self.applicationDataLocation}{QDir.separator()}Notes'
         if not QDir(notesPath).exists():
             appDir.mkdir(notesPath)
-
 
     def text_notes_directory_checker(self):
         appDir = QDir()
@@ -26,13 +25,11 @@ class DirectoryChecker():
         if not QDir(notesPath).exists():
             appDir.mkdir(notesPath)
 
-
     def voice_notes_directory_checker(self):
         appDir = QDir()
         notesPath = f'{self.applicationDataLocation}{QDir.separator()}Notes{QDir.separator()}VoiceNotes'
         if not QDir(notesPath).exists():
             appDir.mkdir(notesPath)
-
 
     def video_notes_directory_checker(self):
         appDir = QDir()
@@ -40,25 +37,110 @@ class DirectoryChecker():
         if not QDir(notesPath).exists():
             appDir.mkdir(notesPath)
 
-
     def paint_notes_directory_checker(self):
         appDir = QDir()
         notesPath = f'{self.applicationDataLocation}{QDir.separator()}Notes{QDir.separator()}PaintNotes'
         if not QDir(notesPath).exists():
             appDir.mkdir(notesPath)
 
-
     def text_notes_directory(self) -> str:
         return f'{self.applicationDataLocation}{QDir.separator()}Notes{QDir.separator()}TextNotes'
-
 
     def voice_notes_directory(self) -> str:
         return f'{self.applicationDataLocation}{QDir.separator()}Notes{QDir.separator()}VoiceNotes'
 
-
     def video_notes_directory(self) -> str:
         return f'{self.applicationDataLocation}{QDir.separator()}Notes{QDir.separator()}VideoNotes'
 
-
     def paint_notes_directory(self) -> str:
         return f'{self.applicationDataLocation}{QDir.separator()}Notes{QDir.separator()}PaintNotes'
+
+    def size_notes_on_disk(self) -> str:
+        dirIter = QDirIterator(f'{self.applicationDataLocation}{QDir.separator()}Notes',
+                               flags=QDirIterator.Subdirectories)
+        totalSize = 0
+        textNotesSize = 0
+        countAndSize = {
+            'total': {
+                'size': 0,
+                'unit': 'Byte',
+                'count': 0,
+            },
+            'text': {
+                'size': 0,
+                'unit': 'Byte',
+                'count': 0,
+            },
+            'voice': {
+                'size': 0,
+                'unit': 'Byte',
+                'count': 0,
+            },
+            'video': {
+                'size': 0,
+                'unit': 'Byte',
+                'count': 0,
+            },
+            'paint': {
+                'size': 0,
+                'unit': 'Byte',
+                'count': 0,
+            },
+        }
+
+        while dirIter.hasNext():
+            dirIter.next()
+            print(dirIter.fileInfo().dir().dirName())
+            if dirIter.fileInfo().dir().dirName() == 'TextNotes':
+                print(dirIter.fileInfo().baseName())
+                if dirIter.fileInfo().isFile():
+                    countAndSize['total']['size'] += dirIter.fileInfo().size()
+                    countAndSize['total']['count'] += 1
+                    countAndSize['text']['size'] += dirIter.fileInfo().size()
+                    countAndSize['text']['count'] += 1
+            if dirIter.fileInfo().dir().dirName() == 'VoiceNotes':
+                print(dirIter.fileInfo().baseName())
+                if dirIter.fileInfo().isFile():
+                    countAndSize['total']['size'] += dirIter.fileInfo().size()
+                    countAndSize['total']['count'] += 1
+                    countAndSize['voice']['size'] += dirIter.fileInfo().size()
+                    countAndSize['voice']['count'] += 1
+            if dirIter.fileInfo().dir().dirName() == 'VideoNotes':
+                print(dirIter.fileInfo().baseName())
+                if dirIter.fileInfo().isFile():
+                    countAndSize['total']['size'] += dirIter.fileInfo().size()
+                    countAndSize['total']['count'] += 1
+                    countAndSize['video']['size'] += dirIter.fileInfo().size()
+                    countAndSize['video']['count'] += 1
+            if dirIter.fileInfo().dir().dirName() == 'PaintNotes':
+                print(dirIter.fileInfo().baseName())
+                if dirIter.fileInfo().isFile():
+                    countAndSize['total']['size'] += dirIter.fileInfo().size()
+                    countAndSize['total']['count'] += 1
+                    countAndSize['paint']['size'] += dirIter.fileInfo().size()
+                    countAndSize['paint']['count'] += 1
+
+        for key, value in countAndSize.items():
+            countAndSize[key] = self.convertSize(countAndSize[key])
+
+        # print(f'text notes size: {textNotesSize}, total notes size: {round(totalSize, 2)} {unit}, files count: {filesCount}')
+        print(f'TOTAL:\n\t{countAndSize['total']}\nTEXT:\n\t{countAndSize['text']}')
+
+        # return {'size': totalSize, 'unit:' unit, }
+
+    def convertSize(self, val: dict):
+        print(val)
+        # Byte to KByte
+        if val['size'] > 1024 and val['unit'] == 'Byte':
+            val['size'] = round(val['size'] / 1024, 2)
+            val['unit'] = 'Kb'
+        # KByte to MByte
+        if val['size'] > 1024 and val['unit'] == 'Kb':
+            val['size'] = round(val['size'] / 1024, 2)
+            val['unit'] = 'Mb'
+        # MByte to GByte
+        if val['size'] > 1024 and val['unit'] == 'Mb':
+            val['size'] = round(val['size'] / 1024, 2)
+            val['unit'] = 'Gb'
+
+        return val
