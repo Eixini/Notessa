61838b528ce2df3b26f63d2d38866055d665ae1e
Eixini <eixi16@gmail.com>
2024-03-11 16:01:19 +0300

    Add listening voice note

---
+++ Notessa/note_windows/ShowVoiceNoteWindow.py
@@ -0,0 +1,100 @@
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect, QTimer,
+    QSize, QTime, QUrl, Qt, QByteArray, QIODevice, QDir, QRegularExpression)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform, QRegularExpressionValidator)
+from PySide6.QtWidgets import (QApplication, QDialog, QGridLayout, QLabel,
+    QPushButton, QSizePolicy, QSpacerItem, QVBoxLayout,
+    QWidget, QMessageBox,)
+from PySide6.QtMultimedia import (QAudioFormat, QAudioSource, QMediaDevices,
+    QAudioSink, QAudioDevice, QMediaFormat, QMediaRecorder, QMediaCaptureSession, QAudioInput, QMediaPlayer, QAudioOutput)
+from PySide6.QtCharts import QChart, QChartView, QLineSeries, QValueAxis
+from Notessa.windows.ui_showvoicenotewindow import Ui_ShowVoiceNoteWindow
+from  Notessa.settings.directory_checker import DirectoryChecker
+from Notessa import rc_icons
+
+
+class ShowVoiceNoteWindow(QDialog):
+    def __init__(self, noteData: list):
+        super().__init__()
+        self.ui = Ui_ShowVoiceNoteWindow()
+        self.ui.setupUi(self)
+
+        self.noteData = noteData
+
+        self.ui.voiceNoteName.setText(self.noteData[1])
+
+        _dir_checker = DirectoryChecker()
+        self._file_path = f'{_dir_checker.voice_notes_directory()}{QDir.separator()}{self.noteData[1]}.{self.noteData[0]}'
+
+        # Media settings
+        self._media_player = QMediaPlayer()
+        self._audio_output = QAudioOutput()
+        self._media_player.setAudioOutput(self._audio_output)
+
+        #
+        self._audio_output.setVolume(0.5)
+        self.ui.volumeSlider.setValue(self._audio_output.volume() * 100)
+
+        # Setting a playback target
+        self._media_player.setSource(QUrl.fromLocalFile(self._file_path))
+
+        # Signal-Slot
+        self.ui.backButton.clicked.connect(self.back)
+        self.ui.playButton.clicked.connect(self.play)
+        self.ui.stopButton.clicked.connect(self.stop)
+        self.ui.pauseButton.clicked.connect(self.pause)
+        self._media_player.positionChanged.connect(self.on_position_changed)
+        self.ui.positionSlider.sliderMoved.connect(self.change_duration_slider)
+        self.ui.volumeSlider.sliderMoved.connect(self.change_volume_slider)
+
+    def back(self):
+        self.reject()
+
+    def play(self):
+
+        # Set the maximum slider value based on the final length of the audio file (msec)
+        duration = self.msec_convert(self._media_player.duration())
+        self.ui.durationLabel.setText(f'{duration['min']}:{duration['sec']}')
+        # Changing the position of the slider depending on the current moment of playback (msec)
+        self.ui.positionSlider.setMaximum(self._media_player.duration())
+
+        # Initialization if the recording has not yet been played or it was reset due to 'Stop'
+        if (self._media_player.playbackState() != QMediaPlayer.PlaybackState.PlayingState and
+                self._media_player.playbackState() != QMediaPlayer.PlaybackState.PausedState):
+            self._media_player.play()
+        else:
+            self._media_player.play()
+
+
+    def stop(self):
+        if self._media_player.playbackState() != QMediaPlayer.PlaybackState.StoppedState:
+            self._media_player.stop()
+
+    def pause(self):
+        if self._media_player.playbackState() != QMediaPlayer.PlaybackState.PausedState:
+            self._media_player.pause()
+
+    def on_position_changed(self):
+        position = self.msec_convert(self._media_player.position())
+        self.ui.currentPositionLabel.setText(f'{position['min']}:{position['sec']}')
+        self.ui.positionSlider.setValue(self._media_player.position())
+
+    def change_duration_slider(self):
+        self._media_player.setPosition(self.ui.positionSlider.value())
+
+    def change_volume_slider(self):
+        self._audio_output.setVolume(self.ui.volumeSlider.value()/100)
+
+    def msec_convert(self, ms):
+        result = {'min': 0, 'sec': 0, 'msec': 0}
+        sec = int(ms/1000)
+        msec = ms % 1000
+        result['sec'] = sec
+        result['msec'] = msec
+        if sec >= 60:
+            result['min'] = int(sec / 60)
+            result['sec'] = sec % 60
+        return result
---
+++ Notessa/windows/ui_showvoicenotewindow.py
@@ -0,0 +1,139 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'ShowVoiceNoteWindow.ui'
+##
+## Created by: Qt User Interface Compiler version 6.6.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QDialog, QGridLayout, QHBoxLayout,
+    QLabel, QLayout, QPushButton, QSizePolicy,
+    QSlider, QVBoxLayout, QWidget)
+
+class Ui_ShowVoiceNoteWindow(object):
+    def setupUi(self, ShowVoiceNoteWindow):
+        if not ShowVoiceNoteWindow.objectName():
+            ShowVoiceNoteWindow.setObjectName(u"ShowVoiceNoteWindow")
+        ShowVoiceNoteWindow.resize(404, 192)
+        sizePolicy = QSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(ShowVoiceNoteWindow.sizePolicy().hasHeightForWidth())
+        ShowVoiceNoteWindow.setSizePolicy(sizePolicy)
+        self.gridLayout = QGridLayout(ShowVoiceNoteWindow)
+        self.gridLayout.setObjectName(u"gridLayout")
+        self.verticalLayout_2 = QVBoxLayout()
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
+        self.verticalLayout_2.setSizeConstraint(QLayout.SetDefaultConstraint)
+        self.voiceNoteName = QLabel(ShowVoiceNoteWindow)
+        self.voiceNoteName.setObjectName(u"voiceNoteName")
+        sizePolicy1 = QSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Fixed)
+        sizePolicy1.setHorizontalStretch(0)
+        sizePolicy1.setVerticalStretch(0)
+        sizePolicy1.setHeightForWidth(self.voiceNoteName.sizePolicy().hasHeightForWidth())
+        self.voiceNoteName.setSizePolicy(sizePolicy1)
+
+        self.verticalLayout_2.addWidget(self.voiceNoteName)
+
+        self.horizontalLayout = QHBoxLayout()
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.currentPositionLabel = QLabel(ShowVoiceNoteWindow)
+        self.currentPositionLabel.setObjectName(u"currentPositionLabel")
+
+        self.horizontalLayout.addWidget(self.currentPositionLabel)
+
+        self.positionSlider = QSlider(ShowVoiceNoteWindow)
+        self.positionSlider.setObjectName(u"positionSlider")
+        self.positionSlider.setOrientation(Qt.Horizontal)
+
+        self.horizontalLayout.addWidget(self.positionSlider)
+
+        self.durationLabel = QLabel(ShowVoiceNoteWindow)
+        self.durationLabel.setObjectName(u"durationLabel")
+
+        self.horizontalLayout.addWidget(self.durationLabel)
+
+
+        self.verticalLayout_2.addLayout(self.horizontalLayout)
+
+        self.horizontalLayout_3 = QHBoxLayout()
+        self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
+        self.volumeLabel = QLabel(ShowVoiceNoteWindow)
+        self.volumeLabel.setObjectName(u"volumeLabel")
+
+        self.horizontalLayout_3.addWidget(self.volumeLabel)
+
+        self.volumeSlider = QSlider(ShowVoiceNoteWindow)
+        self.volumeSlider.setObjectName(u"volumeSlider")
+        self.volumeSlider.setMaximum(100)
+        self.volumeSlider.setOrientation(Qt.Horizontal)
+
+        self.horizontalLayout_3.addWidget(self.volumeSlider)
+
+
+        self.verticalLayout_2.addLayout(self.horizontalLayout_3)
+
+        self.horizontalLayout_2 = QHBoxLayout()
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.playButton = QPushButton(ShowVoiceNoteWindow)
+        self.playButton.setObjectName(u"playButton")
+        sizePolicy2 = QSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Minimum)
+        sizePolicy2.setHorizontalStretch(0)
+        sizePolicy2.setVerticalStretch(0)
+        sizePolicy2.setHeightForWidth(self.playButton.sizePolicy().hasHeightForWidth())
+        self.playButton.setSizePolicy(sizePolicy2)
+
+        self.horizontalLayout_2.addWidget(self.playButton)
+
+        self.pauseButton = QPushButton(ShowVoiceNoteWindow)
+        self.pauseButton.setObjectName(u"pauseButton")
+        sizePolicy2.setHeightForWidth(self.pauseButton.sizePolicy().hasHeightForWidth())
+        self.pauseButton.setSizePolicy(sizePolicy2)
+
+        self.horizontalLayout_2.addWidget(self.pauseButton)
+
+        self.stopButton = QPushButton(ShowVoiceNoteWindow)
+        self.stopButton.setObjectName(u"stopButton")
+        sizePolicy2.setHeightForWidth(self.stopButton.sizePolicy().hasHeightForWidth())
+        self.stopButton.setSizePolicy(sizePolicy2)
+
+        self.horizontalLayout_2.addWidget(self.stopButton)
+
+
+        self.verticalLayout_2.addLayout(self.horizontalLayout_2)
+
+        self.backButton = QPushButton(ShowVoiceNoteWindow)
+        self.backButton.setObjectName(u"backButton")
+
+        self.verticalLayout_2.addWidget(self.backButton)
+
+
+        self.gridLayout.addLayout(self.verticalLayout_2, 0, 0, 1, 1)
+
+
+        self.retranslateUi(ShowVoiceNoteWindow)
+
+        QMetaObject.connectSlotsByName(ShowVoiceNoteWindow)
+    # setupUi
+
+    def retranslateUi(self, ShowVoiceNoteWindow):
+        ShowVoiceNoteWindow.setWindowTitle(QCoreApplication.translate("ShowVoiceNoteWindow", u"Voice note", None))
+        self.voiceNoteName.setText(QCoreApplication.translate("ShowVoiceNoteWindow", u"Voice note name", None))
+        self.currentPositionLabel.setText(QCoreApplication.translate("ShowVoiceNoteWindow", u"00:00", None))
+        self.durationLabel.setText(QCoreApplication.translate("ShowVoiceNoteWindow", u"00:00", None))
+        self.volumeLabel.setText(QCoreApplication.translate("ShowVoiceNoteWindow", u"Volume", None))
+        self.playButton.setText(QCoreApplication.translate("ShowVoiceNoteWindow", u"Play", None))
+        self.pauseButton.setText(QCoreApplication.translate("ShowVoiceNoteWindow", u"Pause", None))
+        self.stopButton.setText(QCoreApplication.translate("ShowVoiceNoteWindow", u"Stop", None))
+        self.backButton.setText(QCoreApplication.translate("ShowVoiceNoteWindow", u"Back", None))
+    # retranslateUi
+
