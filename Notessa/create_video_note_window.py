833646905489698d450799a98243d306b3c88170
Eixini <eixi16@gmail.com>
2024-03-12 18:32:13 +0300

    Add preview option

--- Notessa/create_video_note_window.py
+++ Notessa/create_video_note_window.py
@@ -1,6 +1,6 @@
 from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
-    QMetaObject, QObject, QPoint, QRect, QTimer,
-    QSize, QTime, QUrl, Qt, QByteArray, QIODevice, QDir, QRegularExpression)
+                            QMetaObject, QObject, QPoint, QRect, QTimer,
+                            QSize, QTime, QUrl, Qt, QByteArray, QIODevice, QDir, QRegularExpression, Slot)
 from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
@@ -8,12 +8,16 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
 from PySide6.QtWidgets import (QApplication, QDialog, QGridLayout, QLabel,
     QPushButton, QSizePolicy, QSpacerItem, QVBoxLayout,
     QWidget, QMessageBox,)
-from PySide6.QtMultimedia import (QAudioFormat, QAudioSource, QMediaDevices,
+from PySide6.QtMultimedia import (QAudioFormat, QAudioSource, QMediaDevices, QImageCapture, QVideoFrame,
     QAudioSink, QAudioDevice, QMediaFormat, QMediaRecorder, QMediaCaptureSession, QAudioInput, QCamera, QVideoSink)
-from PySide6.QtCharts import QChart, QChartView, QLineSeries, QValueAxis
 from Notessa.windows.ui_createvideonote import Ui_CreateVideoNoteWindow
-from  Notessa.settings.directory_checker import DirectoryChecker
+from Notessa.settings.directory_checker import DirectoryChecker
 from Notessa import rc_icons
+
+"""
+Mark:
+- check device availability before recording;
+"""
 
 
 class CreateVideoNote(QDialog):
@@ -29,48 +33,110 @@ class CreateVideoNote(QDialog):
         self.ui.stopButton.setIcon(QIcon(':/resource/icons/stop.png'))
         self.ui.backButton.setIcon(QIcon(':/resource/icons/back.png'))
 
+        self.ui.stateLabel.setPixmap(QPixmap(':/resource/icons/video_off.png').scaledToWidth(50).scaledToHeight(50))
+
         # Media settings
         self._capture_session = QMediaCaptureSession()
         self._camera = QCamera()
         self._capture_session.setCamera(self._camera)
         self._media_recorder = QMediaRecorder()
+        self._audio_input = QAudioInput()
         self._capture_session.setRecorder(self._media_recorder)
+        self._capture_session.setAudioInput(self._audio_input)
+        self._media_recorder.setQuality(QMediaRecorder.Quality.VeryHighQuality)
+
         self._video_sink = QVideoSink()
+        self._capture_session.setVideoOutput(self._video_sink)
 
-        self._capture_session.setVideoSink(self._video_sink)
-
-        self._camera.start() # ??????????????
+        self._camera.start()
 
         self._media_format = QMediaFormat(QMediaFormat.FileFormat.MPEG4)
         self._media_format.setVideoCodec(QMediaFormat.VideoCodec.MPEG4)
-        self._media_format.setAudioCodec(QMediaFormat.AudioCodec.MP3)
+        self._media_format.setAudioCodec(QMediaFormat.AudioCodec.Wave)
         self._media_recorder.setMediaFormat(self._media_format)
+
+        #
+        self.ui.videoNoteName.setValidator(QRegularExpressionValidator('([a-zA-Zа-яА-Я0-9-_]){255}'))
 
         # Signal - Slot
         self.ui.recordButton.clicked.connect(self.record)
         self.ui.pauseButton.clicked.connect(self.pause)
         self.ui.stopButton.clicked.connect(self.stop)
         self.ui.backButton.clicked.connect(self.back)
+        self.ui.muteButton.clicked.connect(self.mute)
+
+        self._media_recorder.durationChanged.connect(self.changeLabel)
+        self._media_recorder.recorderStateChanged.connect(self.update_record_state)
+
+        self._video_sink.videoFrameChanged.connect(self.video_frame_changed)
+
+    def video_frame_changed(self):
+        self.ui.videoLabel.setPixmap(QPixmap(self._video_sink.videoFrame().toImage()))
+
+    def update_record_state(self, state):
+        print(f'Record state: {self._media_recorder.recorderState()}')
+        if self._media_recorder.recorderState() == QMediaRecorder.RecorderState.RecordingState:
+            self.ui.recordButton.setEnabled(False)
+            self.ui.pauseButton.setEnabled(True)
+            self.ui.stopButton.setEnabled(True)
+        elif self._media_recorder.recorderState() == QMediaRecorder.RecorderState.PausedState:
+            self.ui.recordButton.setEnabled(True)
+            self.ui.pauseButton.setEnabled(False)
+            self.ui.stopButton.setEnabled(True)
+        elif self._media_recorder.recorderState() == QMediaRecorder.RecorderState.StoppedState:
+            self.ui.recordButton.setEnabled(True)
+            self.ui.pauseButton.setEnabled(False)
+            self.ui.stopButton.setEnabled(False)
+
+    def mute(self):
+        if self._audio_input.isMuted():
+            self._audio_input.setMuted(False)
+            self.ui.muteButton.setText(u'Mute')
+        else:
+            self._audio_input.setMuted(True)
+            self.ui.muteButton.setText(u'Unmute')
 
     def record(self):
-        self.ui.pauseButton.setVisible(True)
-        print(self._media_recorder.recorderState())
-        #
-        datetime = QDateTime().currentDateTime()
-        datetime = datetime.time().msec()
-        #
-        dirChecker = DirectoryChecker()
-        file = f'{dirChecker.video_notes_directory()}{QDir.separator()}{datetime}'
-        url = f'{QDir.toNativeSeparators(file)}'
-        self._media_recorder.setOutputLocation(QUrl.fromLocalFile(url))
+        if (not self.ui.videoNoteName.text() == '' and
+                not self._media_recorder.recorderState() == QMediaRecorder.RecorderState.PausedState):
+            dirChecker = DirectoryChecker()
+            file = f'{dirChecker.video_notes_directory()}{QDir.separator()}{self.ui.videoNoteName.text()}'
+            url = f'{QDir.toNativeSeparators(file)}'
+            self._media_recorder.setOutputLocation(QUrl.fromLocalFile(url))
 
-        self._media_recorder.record()
+            self._media_recorder.record()
+            self.ui.stateLabel.setPixmap(QPixmap(':/resource/icons/recording.png').scaledToWidth(50).scaledToHeight(50))
+        elif self._media_recorder.recorderState() == QMediaRecorder.RecorderState.PausedState:
+            self._media_recorder.record()
+        else:
+            msgBox = QMessageBox()
+            msgBox.setText('Please, enter note name.')
+            msgBox.exec()
 
     def pause(self):
         self._media_recorder.pause()
+        self.ui.stateLabel.setPixmap(QPixmap(':/resource/icons/pause.png').scaledToWidth(50).scaledToHeight(50))
 
     def stop(self):
         self._media_recorder.stop()
+        self.ui.stateLabel.setPixmap(QPixmap(':/resource/icons/video_off.png').scaledToWidth(50).scaledToHeight(50))
+
 
     def back(self):
         self.reject()
+
+    def changeLabel(self):
+        timeDuratin = self.msec_convert(self._media_recorder.duration())
+        if(len(timeDuratin) == 3):
+            self.ui.durationLabel.setText(f'{timeDuratin['min']}:{timeDuratin['sec']}.{timeDuratin['msec']}')
+
+    def msec_convert(self, ms):
+        result = {'min': 0, 'sec': 0, 'msec': 0}
+        sec = int(ms/1000)
+        msec = ms % 1000
+        result['sec'] = sec
+        result['msec'] = msec
+        if sec >= 60:
+            result['min'] = int(sec / 60)
+            result['sec'] = sec % 60
+        return result
