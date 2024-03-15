d095ef5cfbb095bc48abd11e43607fe6f8110588
Eixini <eixi16@gmail.com>
2024-03-15 19:18:22 +0300

    Implemented ability set color and size brush

--- Notessa/paint_note/create_paint_note_window.py
+++ Notessa/paint_note/create_paint_note_window.py
@@ -1,31 +1,68 @@
-from PySide6.QtWidgets import QDialog, QFileDialog
-from PySide6.QtGui import QIcon
-from PySide6.QtCore import Qt, QDir, QStandardPaths
+from PySide6.QtWidgets import QDialog, QFileDialog,  QColorDialog
+from PySide6.QtGui import QIcon, QResizeEvent
+from PySide6.QtCore import QSize, QDir, QStandardPaths
 from Notessa.paint_note.ui_createpaintnote import Ui_CreatePaintNote
 from Notessa.common_modules.directory_checker import DirectoryChecker
 from Notessa.paint_note.painter_widget import PainterWidget
 from Notessa.paint_note.save_messagebox import SaveMessageBox
+from Notessa.paint_note.create_canvas_window import CreateCanvasWindow
+
 from Notessa.resource import rc_icons
 
 class CreatePaintNote(QDialog):
-    def __init__(self):
+    def __init__(self, canvas_size):
         super().__init__()
+
         self.ui = Ui_CreatePaintNote()
         self.ui.setupUi(self)
 
-        self._painter_widget = PainterWidget()
-        self.ui.mdiArea.addSubWindow(self._painter_widget)
-        self.ui.mdiArea.currentSubWindow().setWindowFlags(Qt.FramelessWindowHint)
-        self.setFixedWidth(self._painter_widget.width())
-        self.setFixedHeight(self._painter_widget.height() + self.ui.backButton.height() + self.ui.mainLayout.spacing())
+        # The resulting size value for the canvas
+        self._canvas_size = canvas_size
+        print(canvas_size)
+
+        # WINDOW_FIXED_SIZE = ENTERED_CANVAS_SIZE + (WINDOW_SIZE - ENTERED_CANVAS_SIZE)
+        self.size_diff = QSize(22, 90)
 
         # Icon set
         self.setWindowIcon(QIcon(':/resource/icons/brush.png'))
+
+        self._painter_widget = PainterWidget(self._canvas_size)
+        self.ui.drawingLayout.addWidget(self._painter_widget)
+
+        """ 
+            The recommended minimum window width is 250 px, since there are buttons in the panel.
+        """
+        self.setFixedSize(self._canvas_size + self.size_diff)
+        self.ui.penWidth.setValue(5.0)
+
+        # Color
+        self._color_dialog = QColorDialog(self)
 
         # Signal - Slot
         self.ui.saveButton.clicked.connect(self.save)
         self.ui.openButton.clicked.connect(self.open)
         self.ui.backButton.clicked.connect(self.back)
+
+        self.ui.penWidth.valueChanged.connect(self.pen_width_change)
+        self.ui.colorButton.clicked.connect(self.select_color)
+
+    def resizeEvent(self, event: QResizeEvent):
+        """
+        Designed to obtain the window size.
+        This method was needed in order to calculate the parameters for fixing the window size.
+        To the left of the equal sign, the value that was set after resizing using this event handler.
+        WINDOW_FIXED_SIZE = ENTERED_CANVAS_SIZE + (WINDOW_SIZE - ENTERED_CANVAS_SIZE)
+        """
+        # print(event)
+        pass
+
+    def pen_width_change(self):
+        self._painter_widget.set_pen_width(self.ui.penWidth.value())
+
+    def select_color(self):
+        color = self._color_dialog.getColor()
+        if color.isValid():
+            self._painter_widget.set_pen_color(color)
 
     def save(self):
         note_name = self.enter_note_name()
@@ -43,7 +80,7 @@ class CreatePaintNote(QDialog):
         file_dialog.setDirectory(QStandardPaths.writableLocation(QStandardPaths.StandardLocation.HomeLocation))
         file_dialog.setWindowTitle('Select image')
         file_dialog.setFileMode(QFileDialog.FileMode.ExistingFile)
-        file_dialog.setNameFilter("Images (*.png *.jpeg)")
+        file_dialog.setNameFilter("Images (*.png *.jpeg *.jpg *.bmp *.pbm *.xbm *.xpm *.ppm)")
         file_dialog.setViewMode(QFileDialog.ViewMode.List)
 
         filename = None
@@ -53,10 +90,10 @@ class CreatePaintNote(QDialog):
 
         self._painter_widget.load(filename)
 
-        self.setFixedWidth(self._painter_widget.width())
-        self.setFixedHeight(self._painter_widget.height() + self.ui.backButton.height() + self.ui.mainLayout.spacing())
+        self.setFixedSize(self._painter_widget.size() + self.size_diff)
 
     def enter_note_name(self):
+        """ Method for entering a note name, which will then be used for the note file namea """
         message_box = SaveMessageBox()
         _, note_name = message_box.exec()
         return note_name
--- Notessa/paint_note/ui_createpaintnote.py
+++ Notessa/paint_note/ui_createpaintnote.py
@@ -1,21 +1,69 @@
-from PySide6.QtCore import QCoreApplication, QMetaObject
-from PySide6.QtWidgets import (QGridLayout, QHBoxLayout,
-    QLayout, QMdiArea, QPushButton, QSizePolicy,
-    QSpacerItem, QVBoxLayout)
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'CreatePaintNote.ui'
+##
+## Created by: Qt User Interface Compiler version 6.6.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QDialog, QDoubleSpinBox, QGridLayout,
+    QHBoxLayout, QLabel, QLayout, QPushButton,
+    QSizePolicy, QSpacerItem, QVBoxLayout, QWidget)
 
 class Ui_CreatePaintNote(object):
     def setupUi(self, CreatePaintNote):
         if not CreatePaintNote.objectName():
             CreatePaintNote.setObjectName(u"CreatePaintNote")
-        CreatePaintNote.resize(1081, 710)
+        CreatePaintNote.resize(848, 638)
         self.gridLayout = QGridLayout(CreatePaintNote)
         self.gridLayout.setObjectName(u"gridLayout")
         self.mainLayout = QVBoxLayout()
         self.mainLayout.setObjectName(u"mainLayout")
-        self.mdiArea = QMdiArea(CreatePaintNote)
-        self.mdiArea.setObjectName(u"mdiArea")
+        self.drawingLayout = QVBoxLayout()
+        self.drawingLayout.setObjectName(u"drawingLayout")
 
-        self.mainLayout.addWidget(self.mdiArea)
+        self.mainLayout.addLayout(self.drawingLayout)
+
+        self.verticalSpacer = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
+
+        self.mainLayout.addItem(self.verticalSpacer)
+
+        self.horizontalLayout = QHBoxLayout()
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.horizontalSpacer = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
+
+        self.horizontalLayout.addItem(self.horizontalSpacer)
+
+        self.colorButton = QPushButton(CreatePaintNote)
+        self.colorButton.setObjectName(u"colorButton")
+
+        self.horizontalLayout.addWidget(self.colorButton)
+
+        self.label = QLabel(CreatePaintNote)
+        self.label.setObjectName(u"label")
+
+        self.horizontalLayout.addWidget(self.label)
+
+        self.penWidth = QDoubleSpinBox(CreatePaintNote)
+        self.penWidth.setObjectName(u"penWidth")
+
+        self.horizontalLayout.addWidget(self.penWidth)
+
+        self.toolPanelLayout = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
+
+        self.horizontalLayout.addItem(self.toolPanelLayout)
+
+
+        self.mainLayout.addLayout(self.horizontalLayout)
 
         self.panelLayout = QHBoxLayout()
         self.panelLayout.setSpacing(0)
@@ -64,6 +112,8 @@ class Ui_CreatePaintNote(object):
 
     def retranslateUi(self, CreatePaintNote):
         CreatePaintNote.setWindowTitle(QCoreApplication.translate("CreatePaintNote", u"Create paint note", None))
+        self.colorButton.setText(QCoreApplication.translate("CreatePaintNote", u"Color", None))
+        self.label.setText(QCoreApplication.translate("CreatePaintNote", u"Width", None))
         self.saveButton.setText(QCoreApplication.translate("CreatePaintNote", u"Save", None))
         self.openButton.setText(QCoreApplication.translate("CreatePaintNote", u"Open", None))
         self.backButton.setText(QCoreApplication.translate("CreatePaintNote", u"Back", None))
