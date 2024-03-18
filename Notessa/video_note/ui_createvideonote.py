dbdb66d2fb243b8a859e3d1655eefdfc4fa884ba
Eixini <eixi16@gmail.com>
2024-03-18 22:33:09 +0300

    Add ability to select microphone and camera

--- Notessa/video_note/ui_createvideonote.py
+++ Notessa/video_note/ui_createvideonote.py
@@ -1,18 +1,64 @@
-from PySide6.QtCore import QCoreApplication, QMetaObject, QSize, Qt
-from PySide6.QtWidgets import (QGridLayout, QHBoxLayout,
-    QLabel, QLineEdit, QPushButton, QSizePolicy,
-    QSpacerItem, QVBoxLayout)
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'CreateVideoNote.ui'
+##
+## Created by: Qt User Interface Compiler version 6.6.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtMultimediaWidgets import QVideoWidget
+from PySide6.QtWidgets import (QApplication, QComboBox, QDialog, QGridLayout,
+    QHBoxLayout, QLabel, QLineEdit, QPushButton,
+    QSizePolicy, QSpacerItem, QVBoxLayout, QWidget)
 
 class Ui_CreateVideoNoteWindow(object):
     def setupUi(self, CreateVideoNoteWindow):
         if not CreateVideoNoteWindow.objectName():
             CreateVideoNoteWindow.setObjectName(u"CreateVideoNoteWindow")
         CreateVideoNoteWindow.setWindowModality(Qt.NonModal)
-        CreateVideoNoteWindow.resize(538, 432)
+        CreateVideoNoteWindow.resize(957, 697)
         self.gridLayout = QGridLayout(CreateVideoNoteWindow)
         self.gridLayout.setObjectName(u"gridLayout")
         self.verticalLayout = QVBoxLayout()
         self.verticalLayout.setObjectName(u"verticalLayout")
+        self.horizontalLayout_4 = QHBoxLayout()
+        self.horizontalLayout_4.setObjectName(u"horizontalLayout_4")
+        self.cameraLabel = QLabel(CreateVideoNoteWindow)
+        self.cameraLabel.setObjectName(u"cameraLabel")
+
+        self.horizontalLayout_4.addWidget(self.cameraLabel)
+
+        self.cameraList = QComboBox(CreateVideoNoteWindow)
+        self.cameraList.setObjectName(u"cameraList")
+
+        self.horizontalLayout_4.addWidget(self.cameraList)
+
+        self.horizontalSpacer_6 = QSpacerItem(40, 20, QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Minimum)
+
+        self.horizontalLayout_4.addItem(self.horizontalSpacer_6)
+
+        self.microphoneLabel = QLabel(CreateVideoNoteWindow)
+        self.microphoneLabel.setObjectName(u"microphoneLabel")
+
+        self.horizontalLayout_4.addWidget(self.microphoneLabel)
+
+        self.microphoneList = QComboBox(CreateVideoNoteWindow)
+        self.microphoneList.setObjectName(u"microphoneList")
+
+        self.horizontalLayout_4.addWidget(self.microphoneList)
+
+
+        self.verticalLayout.addLayout(self.horizontalLayout_4)
+
         self.horizontalLayout_3 = QHBoxLayout()
         self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
         self.stateLabel = QLabel(CreateVideoNoteWindow)
@@ -37,16 +83,15 @@ class Ui_CreateVideoNoteWindow(object):
 
         self.verticalLayout.addLayout(self.horizontalLayout_3)
 
-        self.videoLabel = QLabel(CreateVideoNoteWindow)
-        self.videoLabel.setObjectName(u"videoLabel")
+        self.videoDisplay = QVideoWidget(CreateVideoNoteWindow)
+        self.videoDisplay.setObjectName(u"videoDisplay")
         sizePolicy = QSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.videoLabel.sizePolicy().hasHeightForWidth())
-        self.videoLabel.setSizePolicy(sizePolicy)
-        self.videoLabel.setMinimumSize(QSize(0, 0))
+        sizePolicy.setHeightForWidth(self.videoDisplay.sizePolicy().hasHeightForWidth())
+        self.videoDisplay.setSizePolicy(sizePolicy)
 
-        self.verticalLayout.addWidget(self.videoLabel)
+        self.verticalLayout.addWidget(self.videoDisplay)
 
         self.horizontalLayout = QHBoxLayout()
         self.horizontalLayout.setObjectName(u"horizontalLayout")
@@ -58,11 +103,6 @@ class Ui_CreateVideoNoteWindow(object):
         self.recordButton.setObjectName(u"recordButton")
 
         self.horizontalLayout.addWidget(self.recordButton)
-
-        self.pauseButton = QPushButton(CreateVideoNoteWindow)
-        self.pauseButton.setObjectName(u"pauseButton")
-
-        self.horizontalLayout.addWidget(self.pauseButton)
 
         self.stopButton = QPushButton(CreateVideoNoteWindow)
         self.stopButton.setObjectName(u"stopButton")
@@ -115,12 +155,14 @@ class Ui_CreateVideoNoteWindow(object):
 
     def retranslateUi(self, CreateVideoNoteWindow):
         CreateVideoNoteWindow.setWindowTitle(QCoreApplication.translate("CreateVideoNoteWindow", u"Create video note", None))
+        self.cameraLabel.setText(QCoreApplication.translate("CreateVideoNoteWindow", u"Camera", None))
+        self.cameraList.setPlaceholderText(QCoreApplication.translate("CreateVideoNoteWindow", u"Select camera", None))
+        self.microphoneLabel.setText(QCoreApplication.translate("CreateVideoNoteWindow", u"Microphone", None))
+        self.microphoneList.setPlaceholderText(QCoreApplication.translate("CreateVideoNoteWindow", u"Select microphone", None))
         self.stateLabel.setText(QCoreApplication.translate("CreateVideoNoteWindow", u"STATE", None))
         self.durationLabel.setText(QCoreApplication.translate("CreateVideoNoteWindow", u"00:00", None))
         self.videoNoteName.setPlaceholderText(QCoreApplication.translate("CreateVideoNoteWindow", u"Enter video note name ...", None))
-        self.videoLabel.setText(QCoreApplication.translate("CreateVideoNoteWindow", u"TextLabel", None))
         self.recordButton.setText(QCoreApplication.translate("CreateVideoNoteWindow", u"Record", None))
-        self.pauseButton.setText(QCoreApplication.translate("CreateVideoNoteWindow", u"Pause", None))
         self.stopButton.setText(QCoreApplication.translate("CreateVideoNoteWindow", u"Stop", None))
         self.muteButton.setText(QCoreApplication.translate("CreateVideoNoteWindow", u"Mute", None))
         self.backButton.setText(QCoreApplication.translate("CreateVideoNoteWindow", u"Back", None))
