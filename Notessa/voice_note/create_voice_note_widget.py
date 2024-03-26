cfd63856e1fcd4d6dffb7412d8ad01bfab5bb3ca
Eixini <eixi16@gmail.com>
2024-03-26 18:12:59 +0300

    Restructure files and widget

---
+++ Notessa/voice_note/create_voice_note_widget.py
@@ -0,0 +1,157 @@
+from PySide6.QtCore import QUrl, QDir, Qt
+from PySide6.QtGui import QRegularExpressionValidator
+from PySide6.QtWidgets import QDialog, QFrame
+from PySide6.QtMultimedia import QMediaDevices, QMediaFormat, QMediaRecorder, QMediaCaptureSession, QAudioInput
+from Notessa.voice_note.ui_gen.ui_create_voice_note_widget import Ui_CreateVoiceNoteWidget
+from Notessa.common_modules.directory_checker import DirectoryChecker
+
+
+class CreateVoiceNoteWidget(QDialog):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.ui = Ui_CreateVoiceNoteWidget()
+        self.ui.setupUi(self)
+
+        self.setAttribute(Qt.WA_DeleteOnClose)
+        self.installEventFilter(self.parent())
+
+        self.ui.voicenote_name_lineedit.setReadOnly(False)
+        self.ui.pause_button.setEnabled(False)
+
+        self.ui.available_devices_combobox.view().window().setWindowFlags(Qt.Popup | Qt.FramelessWindowHint | Qt.NoDropShadowWindowHint)
+        self.ui.available_devices_combobox.view().window().setAttribute(Qt.WA_TranslucentBackground)
+
+        self.ui.voicenote_name_lineedit.setValidator(QRegularExpressionValidator('([a-zA-Zа-яА-Я0-9-_ ]){255}'))
+
+        # Audio settings
+        self._input_devices = None
+        self._audio_input = QAudioInput(self)
+        self._session = QMediaCaptureSession(self)
+        self._media_recorder = QMediaRecorder(self)
+        self._media_format = QMediaFormat()
+
+        self.microphone_initialization()
+
+        # Signal - Slot
+        self.ui.record_button.clicked.connect(self.record_voice_note)
+        self.ui.stop_button.clicked.connect(self.stop_voice_note)
+        self.ui.pause_button.clicked.connect(self.pause_voice_note)
+
+        self._media_recorder.durationChanged.connect(self.change_label)
+        self._media_recorder.recorderStateChanged.connect(self.update_record_state)
+        self.ui.available_devices_combobox.currentIndexChanged.connect(self.microphone_selection_changed)
+
+        # Signal - Slot
+        self.ui.close_button.clicked.connect(self.close_note)
+
+    def update_record_state(self):
+        """
+        Method for checking the status of a record.
+        The state determines which buttons are available to press.
+        """
+        if self._media_recorder.recorderState() == QMediaRecorder.RecorderState.RecordingState:
+            self.ui.record_button.setEnabled(False)
+            self.ui.pause_button.setEnabled(True)
+            self.ui.stop_button.setEnabled(True)
+        elif self._media_recorder.recorderState() == QMediaRecorder.RecorderState.PausedState:
+            self.ui.record_button.setEnabled(True)
+            self.ui.pause_button.setEnabled(False)
+            self.ui.stop_button.setEnabled(True)
+        elif self._media_recorder.recorderState() == QMediaRecorder.RecorderState.StoppedState:
+            self.ui.record_button.setEnabled(True)
+            self.ui.pause_button.setEnabled(False)
+            self.ui.stop_button.setEnabled(False)
+
+    def microphone_initialization(self):
+        """ Method for initializing the list of available microphones """
+        self._input_devices = QMediaDevices.audioInputs()
+        if len(self._input_devices) > 0:
+            for aud_inp in self._input_devices:
+                self.ui.available_devices_combobox.addItem(aud_inp.description())
+            self._audio_input = QAudioInput(self._input_devices[0])
+
+            self._session.setAudioInput(self._audio_input)
+            self._media_recorder.setQuality(QMediaRecorder.Quality.VeryHighQuality)
+
+            self._media_format.setFileFormat(QMediaFormat.FileFormat.Wave)
+            self._media_format.setAudioCodec(QMediaFormat.AudioCodec.Wave)
+            self._media_recorder.setMediaFormat(self._media_format)
+            self._session.setRecorder(self._media_recorder)
+
+    def microphone_selection_changed(self):
+        """ The method is called when the microphone selection has been changed """
+        index = self.ui.available_devices_combobox.currentIndex()
+        print(f'Current index: {index}, value: {self._input_devices[index].description()}')
+        self._audio_input = QAudioInput(self._input_devices[index])
+        self._session.setAudioInput(self._audio_input)
+
+    def audio_input_changed(self):
+        self._media_recorder.setQuality(QMediaRecorder.Quality.VeryHighQuality)
+
+        self._media_format.setFileFormat(QMediaFormat.FileFormat.Wave)
+        self._media_format.setAudioCodec(QMediaFormat.AudioCodec.Wave)
+        self._media_recorder.setMediaFormat(self._media_format)
+        self._session.setRecorder(self._media_recorder)
+
+    def close_note(self):
+        if self._media_recorder.recorderState() == QMediaRecorder.RecorderState.RecordingState:
+            self._media_recorder.stop()
+        self.close()
+
+    def record_voice_note(self):
+        if (not self.ui.voicenote_name_lineedit.text() == '' and
+                not self._media_recorder.recorderState() == QMediaRecorder.RecorderState.PausedState):
+            # Avoid name change
+            self.ui.voicenote_name_lineedit.setReadOnly(True)
+
+            dir_checker = DirectoryChecker()
+            file = f'{dir_checker.voice_notes_directory()}{QDir.separator()}{self.ui.voicenote_name_lineedit.text()}'
+            url = f'{QDir.toNativeSeparators(file)}'
+            self._media_recorder.setOutputLocation(QUrl.fromLocalFile(url))
+
+            self._media_recorder.record()
+        elif self._media_recorder.recorderState() == QMediaRecorder.RecorderState.PausedState:
+            self._media_recorder.record()
+
+    def pause_voice_note(self):
+        self._media_recorder.pause()
+
+    def stop_voice_note(self):
+        self.ui.stop_button.setEnabled(False)
+        self.ui.record_button.setEnabled(True)
+        self.ui.pause_button.setEnabled(False)
+        self.ui.voicenote_name_lineedit.setReadOnly(False)
+
+        self._media_recorder.stop()
+
+    def closeEvent(self, *args):
+        if (self._media_recorder.recorderState() == QMediaRecorder.RecorderState.PausedState or
+                self._media_recorder.recorderState() == QMediaRecorder.RecorderState.RecordingState):
+            self._media_recorder.stop()
+        self.accept()
+
+    def change_label(self):
+        time_duration = self.msec_convert(self._media_recorder.duration())
+        if len(time_duration) == 3:
+            self.ui.duration_label.setText(f'{time_duration["min"]}:{time_duration["sec"]}.{time_duration["msec"]}')
+
+    def msec_convert(self, ms):
+        result = {'min': 0, 'sec': 0, 'msec': 0}
+        sec = int(ms/1000)
+        msec = ms % 1000
+        result['sec'] = sec
+        result['msec'] = msec
+        if sec >= 60:
+            result['min'] = int(sec / 60)
+            result['sec'] = sec % 60
+        return result
+
+    def __del__(self):
+        # Disconnect Signal - Slot
+        self.ui.record_button.clicked.disconnect(self.record_voice_note)
+        self.ui.stop_button.clicked.disconnect(self.stop_voice_note)
+        self.ui.pause_button.clicked.disconnect(self.pause_voice_note)
+        self._media_recorder.durationChanged.disconnect(self.change_label)
+        self._media_recorder.recorderStateChanged.disconnect(self.update_record_state)
+        self.ui.available_devices_combobox.currentIndexChanged.disconnect(self.microphone_selection_changed)
+        self.ui.close_button.clicked.disconnect(self.close_note())
---
+++ Notessa/voice_note/ui_gen/ui_create_voice_note_widget.py
@@ -0,0 +1,244 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'create_voice_note_widget.ui'
+##
+## Created by: Qt User Interface Compiler version 6.6.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QComboBox, QHBoxLayout, QLabel,
+    QLineEdit, QPushButton, QSizePolicy, QSpacerItem,
+    QVBoxLayout, QWidget)
+from Notessa.resource import icons_rc
+
+class Ui_CreateVoiceNoteWidget(object):
+    def setupUi(self, CreateVoiceNoteWidget):
+        if not CreateVoiceNoteWidget.objectName():
+            CreateVoiceNoteWidget.setObjectName(u"CreateVoiceNoteWidget")
+        CreateVoiceNoteWidget.resize(677, 613)
+        self.verticalLayout = QVBoxLayout(CreateVoiceNoteWidget)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.up_horizontal_layout = QHBoxLayout()
+        self.up_horizontal_layout.setObjectName(u"up_horizontal_layout")
+        self.voicenote_name_lineedit = QLineEdit(CreateVoiceNoteWidget)
+        self.voicenote_name_lineedit.setObjectName(u"voicenote_name_lineedit")
+        self.voicenote_name_lineedit.setFrame(True)
+
+        self.up_horizontal_layout.addWidget(self.voicenote_name_lineedit)
+
+        self.available_devices_label = QLabel(CreateVoiceNoteWidget)
+        self.available_devices_label.setObjectName(u"available_devices_label")
+
+        self.up_horizontal_layout.addWidget(self.available_devices_label)
+
+        self.available_devices_combobox = QComboBox(CreateVoiceNoteWidget)
+        self.available_devices_combobox.setObjectName(u"available_devices_combobox")
+
+        self.up_horizontal_layout.addWidget(self.available_devices_combobox)
+
+
+        self.verticalLayout.addLayout(self.up_horizontal_layout)
+
+        self.verticalSpacer_2 = QSpacerItem(20, 129, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
+
+        self.verticalLayout.addItem(self.verticalSpacer_2)
+
+        self.duration_horizontal_layout = QHBoxLayout()
+        self.duration_horizontal_layout.setObjectName(u"duration_horizontal_layout")
+        self.horizontalSpacer_2 = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
+
+        self.duration_horizontal_layout.addItem(self.horizontalSpacer_2)
+
+        self.duration_label = QLabel(CreateVoiceNoteWidget)
+        self.duration_label.setObjectName(u"duration_label")
+        font = QFont()
+        font.setFamilies([u"Ubuntu"])
+        font.setPointSize(32)
+        font.setBold(True)
+        font.setItalic(True)
+        font.setStrikeOut(False)
+        self.duration_label.setFont(font)
+
+        self.duration_horizontal_layout.addWidget(self.duration_label)
+
+        self.horizontalSpacer_3 = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
+
+        self.duration_horizontal_layout.addItem(self.horizontalSpacer_3)
+
+
+        self.verticalLayout.addLayout(self.duration_horizontal_layout)
+
+        self.verticalSpacer_3 = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
+
+        self.verticalLayout.addItem(self.verticalSpacer_3)
+
+        self.media_buttons_horizontal_layout = QHBoxLayout()
+        self.media_buttons_horizontal_layout.setObjectName(u"media_buttons_horizontal_layout")
+        self.horizontalSpacer_4 = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
+
+        self.media_buttons_horizontal_layout.addItem(self.horizontalSpacer_4)
+
+        self.pause_button = QPushButton(CreateVoiceNoteWidget)
+        self.pause_button.setObjectName(u"pause_button")
+        self.pause_button.setEnabled(True)
+        sizePolicy = QSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Minimum)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.pause_button.sizePolicy().hasHeightForWidth())
+        self.pause_button.setSizePolicy(sizePolicy)
+        palette = QPalette()
+        brush = QBrush(QColor(0, 0, 0, 255))
+        brush.setStyle(Qt.SolidPattern)
+        palette.setBrush(QPalette.Active, QPalette.WindowText, brush)
+        brush1 = QBrush(QColor(255, 255, 255, 255))
+        brush1.setStyle(Qt.SolidPattern)
+        palette.setBrush(QPalette.Active, QPalette.Button, brush1)
+        palette.setBrush(QPalette.Active, QPalette.Light, brush1)
+        palette.setBrush(QPalette.Active, QPalette.Midlight, brush1)
+        brush2 = QBrush(QColor(127, 127, 127, 255))
+        brush2.setStyle(Qt.SolidPattern)
+        palette.setBrush(QPalette.Active, QPalette.Dark, brush2)
+        brush3 = QBrush(QColor(170, 170, 170, 255))
+        brush3.setStyle(Qt.SolidPattern)
+        palette.setBrush(QPalette.Active, QPalette.Mid, brush3)
+        palette.setBrush(QPalette.Active, QPalette.Text, brush)
+        palette.setBrush(QPalette.Active, QPalette.BrightText, brush1)
+        palette.setBrush(QPalette.Active, QPalette.ButtonText, brush)
+        palette.setBrush(QPalette.Active, QPalette.Base, brush1)
+        palette.setBrush(QPalette.Active, QPalette.Window, brush1)
+        palette.setBrush(QPalette.Active, QPalette.Shadow, brush)
+        palette.setBrush(QPalette.Active, QPalette.AlternateBase, brush1)
+        brush4 = QBrush(QColor(255, 255, 220, 255))
+        brush4.setStyle(Qt.SolidPattern)
+        palette.setBrush(QPalette.Active, QPalette.ToolTipBase, brush4)
+        palette.setBrush(QPalette.Active, QPalette.ToolTipText, brush)
+        brush5 = QBrush(QColor(0, 0, 0, 127))
+        brush5.setStyle(Qt.SolidPattern)
+#if QT_VERSION >= QT_VERSION_CHECK(5, 12, 0)
+        palette.setBrush(QPalette.Active, QPalette.PlaceholderText, brush5)
+#endif
+        palette.setBrush(QPalette.Inactive, QPalette.WindowText, brush)
+        palette.setBrush(QPalette.Inactive, QPalette.Button, brush1)
+        palette.setBrush(QPalette.Inactive, QPalette.Light, brush1)
+        palette.setBrush(QPalette.Inactive, QPalette.Midlight, brush1)
+        palette.setBrush(QPalette.Inactive, QPalette.Dark, brush2)
+        palette.setBrush(QPalette.Inactive, QPalette.Mid, brush3)
+        palette.setBrush(QPalette.Inactive, QPalette.Text, brush)
+        palette.setBrush(QPalette.Inactive, QPalette.BrightText, brush1)
+        palette.setBrush(QPalette.Inactive, QPalette.ButtonText, brush)
+        palette.setBrush(QPalette.Inactive, QPalette.Base, brush1)
+        palette.setBrush(QPalette.Inactive, QPalette.Window, brush1)
+        palette.setBrush(QPalette.Inactive, QPalette.Shadow, brush)
+        palette.setBrush(QPalette.Inactive, QPalette.AlternateBase, brush1)
+        palette.setBrush(QPalette.Inactive, QPalette.ToolTipBase, brush4)
+        palette.setBrush(QPalette.Inactive, QPalette.ToolTipText, brush)
+#if QT_VERSION >= QT_VERSION_CHECK(5, 12, 0)
+        palette.setBrush(QPalette.Inactive, QPalette.PlaceholderText, brush5)
+#endif
+        palette.setBrush(QPalette.Disabled, QPalette.WindowText, brush2)
+        palette.setBrush(QPalette.Disabled, QPalette.Button, brush1)
+        palette.setBrush(QPalette.Disabled, QPalette.Light, brush1)
+        palette.setBrush(QPalette.Disabled, QPalette.Midlight, brush1)
+        palette.setBrush(QPalette.Disabled, QPalette.Dark, brush2)
+        palette.setBrush(QPalette.Disabled, QPalette.Mid, brush3)
+        palette.setBrush(QPalette.Disabled, QPalette.Text, brush2)
+        palette.setBrush(QPalette.Disabled, QPalette.BrightText, brush1)
+        palette.setBrush(QPalette.Disabled, QPalette.ButtonText, brush2)
+        palette.setBrush(QPalette.Disabled, QPalette.Base, brush1)
+        palette.setBrush(QPalette.Disabled, QPalette.Window, brush1)
+        palette.setBrush(QPalette.Disabled, QPalette.Shadow, brush)
+        palette.setBrush(QPalette.Disabled, QPalette.AlternateBase, brush1)
+        palette.setBrush(QPalette.Disabled, QPalette.ToolTipBase, brush4)
+        palette.setBrush(QPalette.Disabled, QPalette.ToolTipText, brush)
+        brush6 = QBrush(QColor(127, 127, 127, 127))
+        brush6.setStyle(Qt.SolidPattern)
+#if QT_VERSION >= QT_VERSION_CHECK(5, 12, 0)
+        palette.setBrush(QPalette.Disabled, QPalette.PlaceholderText, brush6)
+#endif
+        self.pause_button.setPalette(palette)
+        icon = QIcon()
+        icon.addFile(u":/icons/pause.png", QSize(), QIcon.Normal, QIcon.Off)
+        self.pause_button.setIcon(icon)
+        self.pause_button.setIconSize(QSize(32, 32))
+
+        self.media_buttons_horizontal_layout.addWidget(self.pause_button)
+
+        self.record_button = QPushButton(CreateVoiceNoteWidget)
+        self.record_button.setObjectName(u"record_button")
+        sizePolicy.setHeightForWidth(self.record_button.sizePolicy().hasHeightForWidth())
+        self.record_button.setSizePolicy(sizePolicy)
+        icon1 = QIcon()
+        icon1.addFile(u":/icons/play.png", QSize(), QIcon.Normal, QIcon.Off)
+        self.record_button.setIcon(icon1)
+        self.record_button.setIconSize(QSize(32, 32))
+
+        self.media_buttons_horizontal_layout.addWidget(self.record_button)
+
+        self.stop_button = QPushButton(CreateVoiceNoteWidget)
+        self.stop_button.setObjectName(u"stop_button")
+        sizePolicy.setHeightForWidth(self.stop_button.sizePolicy().hasHeightForWidth())
+        self.stop_button.setSizePolicy(sizePolicy)
+        icon2 = QIcon()
+        icon2.addFile(u":/icons/stop.png", QSize(), QIcon.Normal, QIcon.Off)
+        self.stop_button.setIcon(icon2)
+        self.stop_button.setIconSize(QSize(32, 32))
+
+        self.media_buttons_horizontal_layout.addWidget(self.stop_button)
+
+        self.horizontalSpacer_5 = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
+
+        self.media_buttons_horizontal_layout.addItem(self.horizontalSpacer_5)
+
+
+        self.verticalLayout.addLayout(self.media_buttons_horizontal_layout)
+
+        self.verticalSpacer = QSpacerItem(20, 129, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
+
+        self.verticalLayout.addItem(self.verticalSpacer)
+
+        self.buttons_panel_horizontal_layout = QHBoxLayout()
+        self.buttons_panel_horizontal_layout.setObjectName(u"buttons_panel_horizontal_layout")
+        self.close_button = QPushButton(CreateVoiceNoteWidget)
+        self.close_button.setObjectName(u"close_button")
+        sizePolicy.setHeightForWidth(self.close_button.sizePolicy().hasHeightForWidth())
+        self.close_button.setSizePolicy(sizePolicy)
+        icon3 = QIcon()
+        icon3.addFile(u":/icons/back.png", QSize(), QIcon.Normal, QIcon.Off)
+        self.close_button.setIcon(icon3)
+        self.close_button.setIconSize(QSize(32, 32))
+
+        self.buttons_panel_horizontal_layout.addWidget(self.close_button)
+
+        self.horizontalSpacer = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
+
+        self.buttons_panel_horizontal_layout.addItem(self.horizontalSpacer)
+
+
+        self.verticalLayout.addLayout(self.buttons_panel_horizontal_layout)
+
+
+        self.retranslateUi(CreateVoiceNoteWidget)
+
+        QMetaObject.connectSlotsByName(CreateVoiceNoteWidget)
+    # setupUi
+
+    def retranslateUi(self, CreateVoiceNoteWidget):
+        CreateVoiceNoteWidget.setWindowTitle(QCoreApplication.translate("CreateVoiceNoteWidget", u"Form", None))
+        self.voicenote_name_lineedit.setPlaceholderText(QCoreApplication.translate("CreateVoiceNoteWidget", u"Enter note name...", None))
+        self.available_devices_label.setText(QCoreApplication.translate("CreateVoiceNoteWidget", u"Available devices", None))
+        self.duration_label.setText(QCoreApplication.translate("CreateVoiceNoteWidget", u"0:00", None))
+        self.pause_button.setText("")
+        self.record_button.setText("")
+        self.stop_button.setText("")
+        self.close_button.setText(QCoreApplication.translate("CreateVoiceNoteWidget", u"Back", None))
+    # retranslateUi
+
